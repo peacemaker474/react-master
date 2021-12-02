f82358936f48c0f5aad1f8b533edae1ee6f1cf3e
{hc456123@gmail.com} <hc456123@gmail.com>
2021-12-02 20:07:18 +0900

    2021-12-02 React and TS setting

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Router from "./routes/Router";
+import GlobalStyle from "./styles/GlobalStyles";
+
+
+function App () {
+  return (
+    <>
+      <GlobalStyle />
+      <Router />
+    </>
+  )
+}
+
+export default App;
