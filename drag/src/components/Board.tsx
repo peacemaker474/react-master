e1b65b21e134a0d0b6ec431d61fd58bb9f5bb9a1
{hc456123@gmail.com} <hc456123@gmail.com>
2021-12-08 00:03:17 +0900

    2021-12-08 ToDo List drag and drop function

--- drag/src/components/Board.tsx
+++ drag/src/components/Board.tsx
@@ -1,14 +1,32 @@
 import { Droppable } from 'react-beautiful-dnd';
 import DragabbleCard from './DragabbleCard';
 import styled from 'styled-components';
+import { useForm } from 'react-hook-form';
+import { IToDo, toDoState } from '../atoms';
+import { useSetRecoilState } from 'recoil';
+
+interface IBoardProps {
+    toDos: IToDo[];
+    boardId: string;
+}
+
+interface IAreaProps {
+    isDraggingFromThis: boolean;
+    isDraggingOver: boolean;
+}
+
+interface IForm {
+    toDo: string;
+}
 
 const Wrapper = styled.div`
     background-color: ${props => props.theme.boardColor};
     width: 300px;
-    padding: 20px 10px;
     padding-top: 10px;
     border-radius: 5px;
     min-height: 300px;
+    display: flex;
+    flex-direction: column;
 `;
 
 const Title = styled.h2`
@@ -18,23 +36,65 @@ const Title = styled.h2`
     font-size: 18px;
 `;
 
-interface IBoardProps {
-    toDos: string[];
-    boardId: string;
-}
+const Area = styled.div<IAreaProps>`
+    background-color: ${props => 
+        props.isDraggingOver ? "#dfe6e9" : props.isDraggingFromThis ? "#b2bec3" : "transparent"};
+    flex-grow: 1;
+    transition: background-color .3s ease-in-out;
+    padding: 20px;
+`;
+
+const Form = styled.form`
+    width: 100%;
+    input {
+        all: unset;
+        width: 100%;
+        height: 30px;
+        padding-left: 10px;
+        background-color: white;
+        box-sizing: border-box;
+    }
+`;
 
 function Board({toDos, boardId}: IBoardProps) {
+    const setToDos = useSetRecoilState(toDoState);
+    const { register, handleSubmit, setValue } = useForm<IForm>();
+    const onVaild = ({ toDo }: IForm) => {
+        const newToDo = {
+            id: Date.now(),
+            text: toDo,
+        };
+        setToDos(allBoards => {
+            return {
+                ...allBoards,
+                [boardId]: [newToDo, ...allBoards[boardId]]
+            }
+        })
+        setValue("toDo", "");
+    }
     return (
         <Wrapper>
             <Title> {boardId} </Title>
+            <Form onSubmit={handleSubmit(onVaild)}>
+                <input 
+                    {...register("toDo", { required: true })}
+                    type="text"
+                    placeholder={`Add task on ${boardId}`}
+                />
+            </Form>
             <Droppable droppableId={boardId}>
-            {(magic) => 
-                <div ref={magic.innerRef} {...magic.droppableProps}>
+            {(provided, info) => 
+                <Area 
+                    isDraggingOver={info.isDraggingOver}
+                    isDraggingFromThis={Boolean(info.draggingFromThisWith)}
+                    ref={provided.innerRef}
+                    {...provided.droppableProps}
+                >
                     {toDos.map((toDo, index) => (
-                        <DragabbleCard key={toDo} toDo={toDo} index={index}/>
+                        <DragabbleCard key={toDo.id} toDoId={toDo.id} toDoText={toDo.text} index={index}/>
                     ))}
-                {magic.placeholder}
-                </div>
+                {provided.placeholder}
+                </Area>
             }
             </Droppable>
         </Wrapper>
--- drag/src/components/DragabbleCard.tsx
+++ drag/src/components/DragabbleCard.tsx
@@ -2,29 +2,31 @@ import React from 'react';
 import { Draggable } from "react-beautiful-dnd";
 import styled from "styled-components";
 
-const Card = styled.div`
-    background-color: ${props => props.theme.cardColor};
+const Card = styled.div<{isDragging: boolean}>`
+    background-color: ${props => props.isDragging ? "#74be99" : props.theme.cardColor};
     margin-bottom: 10px;
     border-radius: 5px;
     padding: 10px 10px;
+    box-shadow: ${props => props.isDragging ? "0px 2px 5px rgba(0, 0, 0, .05)" : "none"}
 `;
 
 interface IDragabbleCardProps {
-    toDo: string;
+    toDoId: number;
+    toDoText: string;
     index: number;
 }
 
-function DragabbleCard ({toDo, index}: IDragabbleCardProps) {
-    console.log(toDo);
+function DragabbleCard ({toDoId, toDoText, index}: IDragabbleCardProps) {
     return (
-        <Draggable key={toDo} draggableId={toDo} index={index}>
-            {(provided) => (
+        <Draggable key={toDoId} draggableId={toDoId + ""} index={index}>
+            {(provided, snapshot) => (
             <Card 
+                isDragging={snapshot.isDragging}
                 ref={provided.innerRef}
                 {...provided.draggableProps}
                 {...provided.dragHandleProps}
             >
-                {toDo}
+                {toDoText}
             </Card>
             )}
         </Draggable>
